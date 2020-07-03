caf1185c7f42e5e5f7898a71c8b49a1105d348f3
Stas <stanislavkviz@mail.ru>
2020-07-03 16:37:11 +0300

    solved task 3602

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3602/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task36.task3602;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Collections;
+import java.util.List;
+
+/*
+Найти класс по описанию Ӏ Java Collections: 6 уровень, 6 лекция
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+
+        Class[] classes = Collections.class.getDeclaredClasses();
+
+        for(Class currentClass: classes){
+            if(Modifier.isPrivate(currentClass.getModifiers())){
+                if(Modifier.isStatic(currentClass.getModifiers())){
+                    if(List.class.isAssignableFrom(currentClass)){
+                    try{
+                        Constructor constructor = currentClass.getDeclaredConstructor();
+                        constructor.setAccessible(true);
+                        List list = (List) constructor.newInstance();
+                        list.get(0);
+                    }catch (IndexOutOfBoundsException e){
+                        return currentClass;
+                    } catch (InstantiationException | NoSuchMethodException |
+                            InvocationTargetException | IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    }
+                }
+            }
+        }
+
+    return null;
+    }
+}
